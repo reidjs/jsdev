c3c3efe2f5b898cc2f7e3e7f15ff5f8dca8aa98c
reid <reidsherman@gmail.com>
2017-08-17 08:58:33 -0400

    jquery events added

---
+++ methodscript.js
@@ -0,0 +1,21 @@
+$("li").click(function(){
+  $(this).css("color", "red")
+  console.log("clicked on " + $(this).text())
+})
+$("input").keypress(function(e){
+  //jQuery.event.which tells you the ASCII key value
+  if (e.which == 13) console.log("enter pressed")
+})
+$("h1").on("click", function(e){
+  console.log(e)
+  $(this).css("color","purple")
+})
+$("input").on("keypress", function(){
+  console.log("key pressed")
+})
+$("li").on("mouseenter", function(){
+  $(this).css("font-weight", "bold")
+})
+$("li").on("mouseleave", function(){
+  $(this).css("font-weight", "normal")
+})
