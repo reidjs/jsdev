26a3c26416c17bbfa45dc52369d42ca195a1d7eb
reid <reidsherman@gmail.com>
2017-08-23 07:35:36 -0400

    added data persistence with mongo db

--- yelpcamp/app.js
+++ yelpcamp/app.js
@@ -1,27 +1,53 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+var express = require('express'),
+  bodyParser = require('body-parser'),
+  mongoose = require('mongoose');
 app = express();
+mongoose.connect("mongodb://localhost/yelp_camp");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
-var campgrounds = [
-  {name: "Salmon Creek", image: "http://haulihuvila.com/wp-content/uploads/2012/09/hauli-huvila-campgrounds-lg.jpg"},
-  {name: "Mountain Goat Hill", image: "http://visitindianacountypa.org/wp-content/themes/indianna/images/graphics/wheel%20in%20campground%20(2).jpg"},
-  {name: "Salmon Creek", image: "http://haulihuvila.com/wp-content/uploads/2012/09/hauli-huvila-campgrounds-lg.jpg"},
-  {name: "Mountain Goat Hill", image: "http://visitindianacountypa.org/wp-content/themes/indianna/images/graphics/wheel%20in%20campground%20(2).jpg"},
-  {name: "Salmon Creek", image: "http://haulihuvila.com/wp-content/uploads/2012/09/hauli-huvila-campgrounds-lg.jpg"},
-  {name: "Mountain Goat Hill", image: "http://visitindianacountypa.org/wp-content/themes/indianna/images/graphics/wheel%20in%20campground%20(2).jpg"},
-  {name: "Salmon Creek", image: "http://haulihuvila.com/wp-content/uploads/2012/09/hauli-huvila-campgrounds-lg.jpg"},
-  {name: "Mountain Goat Hill", image: "http://visitindianacountypa.org/wp-content/themes/indianna/images/graphics/wheel%20in%20campground%20(2).jpg"},
-  {name: "Salmon Creek", image: "http://haulihuvila.com/wp-content/uploads/2012/09/hauli-huvila-campgrounds-lg.jpg"},
-  {name: "Mountain Goat Hill", image: "http://visitindianacountypa.org/wp-content/themes/indianna/images/graphics/wheel%20in%20campground%20(2).jpg"},
-  {name: "Mountain Valley", image: "http://camprrm.com/wp-content/uploads/2011/06/whiteface1.jpg"}
-];
+//schema for mongoose
+var campgroundSchema = new mongoose.Schema({
+  name: String,
+  image: String
+});
+var Campground = mongoose.model("Campground", campgroundSchema);
+
+// Campground.create({
+//   name: "Mountain Goat Hill",
+//   image: "http://visitindianacountypa.org/wp-content/themes/indianna/images/graphics/wheel%20in%20campground%20(2).jpg"
+// }, function(err, campground){
+//   if (err)
+//     console.log(err);
+//   else
+//     console.log("NEW CAMPGROUND: ",campground);
+// });
+
+
+// var campgrounds = [
+//   {name: "Salmon Creek", image: "http://haulihuvila.com/wp-content/uploads/2012/09/hauli-huvila-campgrounds-lg.jpg"},
+//   {name: "Mountain Goat Hill", image: "http://visitindianacountypa.org/wp-content/themes/indianna/images/graphics/wheel%20in%20campground%20(2).jpg"},
+//   {name: "Salmon Creek", image: "http://haulihuvila.com/wp-content/uploads/2012/09/hauli-huvila-campgrounds-lg.jpg"},
+//   {name: "Mountain Goat Hill", image: "http://visitindianacountypa.org/wp-content/themes/indianna/images/graphics/wheel%20in%20campground%20(2).jpg"},
+//   {name: "Salmon Creek", image: "http://haulihuvila.com/wp-content/uploads/2012/09/hauli-huvila-campgrounds-lg.jpg"},
+//   {name: "Mountain Goat Hill", image: "http://visitindianacountypa.org/wp-content/themes/indianna/images/graphics/wheel%20in%20campground%20(2).jpg"},
+//   {name: "Salmon Creek", image: "http://haulihuvila.com/wp-content/uploads/2012/09/hauli-huvila-campgrounds-lg.jpg"},
+//   {name: "Mountain Goat Hill", image: "http://visitindianacountypa.org/wp-content/themes/indianna/images/graphics/wheel%20in%20campground%20(2).jpg"},
+//   {name: "Salmon Creek", image: "http://haulihuvila.com/wp-content/uploads/2012/09/hauli-huvila-campgrounds-lg.jpg"},
+//   {name: "Mountain Goat Hill", image: "http://visitindianacountypa.org/wp-content/themes/indianna/images/graphics/wheel%20in%20campground%20(2).jpg"},
+//   {name: "Mountain Valley", image: "http://camprrm.com/wp-content/uploads/2011/06/whiteface1.jpg"}
+// ];
 app.get("/", function(req, res){
   res.render("landing");
 });
 app.get("/campgrounds", function(req, res){
-
-  res.render("campgrounds", {campgrounds: campgrounds});
+  Campground.find({}, function(err, allCampgrounds){
+    if (err)
+      console.log(err);
+    else {
+      res.render("campgrounds", {campgrounds:allCampgrounds});
+    }
+  });
+  //res.render("campgrounds", {campgrounds: campgrounds});
 });
 //To make routes RESTful, we use identical URLs for GET and POST
 app.post("/campgrounds", function(req, res){
@@ -30,8 +56,17 @@ app.post("/campgrounds", function(req, res){
   var name = req.body.name;
   var image = req.body.image;
   var newCampground = {name: name, image: image};
-  campgrounds.push(newCampground);
-  res.redirect("/campgrounds"); //automatically redirects as GET
+  Campground.create(newCampground, function(err, newlyCreate){
+    if (err){
+      console.log(err);
+    }
+    else {
+      res.redirect("/campgrounds"); //automatically redirects as GET
+    }
+  });
+  //campgrounds.push(newCampground);
+  //create new campground and save to DB
+
 });
 app.get("/campgrounds/new", function(req, res){
   res.render('new.ejs');
