00946a2aff05580ccbd643b7c7a64292feea7756
reid <reidsherman@gmail.com>
2017-08-23 16:07:09 -0400

    added UPDATE restful architecture to blog

---
+++ restfulrouting/restfulblogapp/app.js
@@ -0,0 +1,93 @@
+var express = require('express'),
+  app = express(),
+  bodyParser = require('body-parser'),
+  mongoose = require('mongoose');
+  methodOverride = require("method-override");
+//App config
+mongoose.connect("mongodb://localhost/restful_blog_app");
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(methodOverride("_method")); //to fix form UPDATE/DELETE add action ?_method=PUT or ?_method=DELETE
+app.use(bodyParser.urlencoded({extended:true}));
+//Mongoose/Model config
+var blogSchema = new mongoose.Schema({
+  title: String,
+  image: String,
+  body: String,
+  created: {type: Date, default: Date.now}
+});
+var Blog = mongoose.model("Blog", blogSchema);
+
+//Restful Routes
+// Blog.create({
+//   title: "Test Blog",
+//   image: "https://i.pinimg.com/236x/45/ed/cc/45edccf87f211186b55e7db9b8ecba67--hot-dogs-random-pictures.jpg",
+//   body: "This is a blog post"
+// });
+//INDEX route
+app.get("/", function(req, res){
+  res.redirect("/blogs");
+});
+app.get("/blogs", function(req, res){
+  Blog.find({}, function(err, blogs){
+    if(err)
+      console.log(err);
+    else {
+      res.render("index", {blogs: blogs});
+    }
+  });
+});
+//NEW Route
+app.get("/blogs/new", function(req, res){
+  res.render("new");
+});
+//CREATE route
+app.post("/blogs", function(req, res){
+  //create blog
+  Blog.create(req.body.blog, function(err, newBlog){
+    if (err) {
+      res.render("new");
+    }
+    else{
+      res.redirect("/blogs");
+    }
+  });
+  //redirect to index
+});
+//SHOW route
+app.get("/blogs/:id", function(req, res){
+  Blog.findById(req.params.id, function(err, foundBlog){
+    if(err){
+      res.redirect("/blogs");
+    }
+    else {
+      res.render("show", {blog: foundBlog});
+    }
+  });
+});
+//EDIT route
+app.get("/blogs/:id/edit", function(req, res){
+  Blog.findById(req.params.id, function(err, foundBlog){
+    if(err)
+      res.redirect("/blogs");
+    else {
+      res.render("edit", {blog: foundBlog});
+    }
+  });
+  // res.render("edit");
+});
+//UPDATE Route
+app.put("/blogs/:id", function(req, res){
+  Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog){
+    if(err){
+      res.redirect("/blogs");
+    }
+    else {
+      res.redirect("/blogs/"+req.params.id); //redirect to showpage for blog post
+    }
+  });
+});
+
+app.listen(3000, function(){
+  console.log("server is running");
+});
