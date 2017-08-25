de6b43a28fc7a694e2d2db220b1652ed43b7152a
reid <reidsherman@gmail.com>
2017-08-25 08:14:10 -0400

    stylized show page

--- yelpcamp/seeds.js
+++ yelpcamp/seeds.js
@@ -3,19 +3,19 @@ var Campground = require('./models/campground');
 var myComment = require('./models/comment');
 var data = [
   {
-    name: "so and so",
+    name: "Campground Name",
     image: "https://farm1.staticflickr.com/130/321487195_ff34bde2f5.jpg",
-    description: "asdf"
+    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
   },
   {
-    name: "hi there",
+    name: "Rolling Rock",
     image: "https://farm6.staticflickr.com/5181/5641024448_04fefbb64d.jpg",
-    description: "asdf"
+    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
   },
   {
-    name: "camp 3",
+    name: "Seirra Nevada",
     image: "https://farm3.staticflickr.com/2535/3823437635_c712decf64.jpg",
-    description: "asdf"
+    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
   }
 ];
 function seedDB(){
